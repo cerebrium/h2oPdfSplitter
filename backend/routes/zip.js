210ca1d5772b4aa9ff8f7d749676ed40510b977d
Nicholas Shankland <nicholas.m.shankland@gmail.com>
2022-03-02 19:59:36 -0800

    revert

--- backend/routes/zip.js
+++ backend/routes/zip.js
@@ -3,64 +3,89 @@ var router = express.Router();
 var JSZip = require("jszip");
 var fetch = require("node-fetch");
 var fs = require("fs");
+var path = require("path");
 
 // basic get route that tells you what to do
 router.get("/", function (req, res) {
-  res.status(200).send("Hello World")
+  // let pathToPdf = path.join(__dirname, '/../pdfs.zip')
+  // res.sendFile(pathToPdf)
 });
 
 // post route that does the zippingl
 router.post("/", function (req, res) {
-  if (!Array.isArray(req.body.files)) {
-    res.status(400).json({
-      error: "Please be sure that the type is []Array",
-    });
-  }
-  if (req.body.files.length === 0) {
-    res.status(400).json({
-      error: "There is no data in your array",
-    });
-  } 
-  try {
-    renameAndZip(req.body.names, req.body.files);
-  } catch (e) {
-    res.status(299).json({e});
-  }
-
-  // Function call
-  async function renameAndZip(nameAttay, pdfArray) {
-    var zip = new JSZip();
-    var pdfZip = zip.folder("pdfs");
-
-    const getData = async function (urlNum, url) {
-      await fetch(url)
-        .then((response) => response.blob())
-        .then((response) => response.arrayBuffer())
-        .then((response) => {
-          pdfZip.file(`${nameAttay[urlNum]}.pdf`, response, {
-            base64: true,
-          });
-          try {
-            pdfZip
-              .generateNodeStream({
-                type: "nodebuffer",
-                streamFiles: true,
-              })
-              .pipe(fs.createWriteStream("pdfs.zip"))
-              .on("finish", function () {
-                // JSZip generates a readable stream with a "end" event,
-                // but is piped here in a writable stream which emits a "finish" event.
-              });
-          } catch (error) {
-            console.error(error);
-          }
+  console.log("inside post: ", req.body);
+  if (req.body.files && req.body.names) {
+    if (Array.isArray(req.body.files)) {
+      console.log("is an array");
+      if (req.body.files.length === 0) {
+        res.status(400).json({
+          error: "There is no data in your array",
         });
-    };
+      } else {
+        async function renameAndZip(nameAttay, pdfArray) {
+          var zip = new JSZip();
+          var pdfZip = zip.folder("pdfs");
 
-    const finalizedPdfArray = await Promise.all(pdfArray.map((pdf, pdfnum) => {
-      getData(pdfnum, pdf);
-    }));
-    res.status(200).send("ready for fetching");
+          let getData = async function (urlNum, url) {
+            await fetch(url)
+              .then((response) => response.blob())
+              .then((response) => response.arrayBuffer())
+              .then((response) => {
+                console.log(
+                  "this is the response:",
+                  response,
+                  "\n",
+                  urlNum,
+                  "\n",
+                  url
+                );
+                pdfZip.file(`${nameAttay[urlNum]}.pdf`, response, {
+                  base64: true,
+                });
+                try {
+                  pdfZip
+                    .generateNodeStream({
+                      type: "nodebuffer",
+                      streamFiles: true,
+                    })
+                    .pipe(fs.createWriteStream("pdfs.zip"))
+                    .on("finish", function () {
+                      // JSZip generates a readable stream with a "end" event,
+                      // but is piped here in a writable stream which emits a "finish" event.
+                    });
+                } catch (error) {
+                  console.log(error);
+                }
+              });
+          };
+
+          pdfArray.forEach((pdf, pdfnum) => {
+            getData(pdfnum, pdf);
+          });
+          setTimeout(() => {
+            // trying to send the zip
+            // let pathToPdf = path.join(__dirname, '/../pdfs.zip')
+
+            // res.sendFile(pathToPdf)
+            // res.send('hello')
+            res.status(200).send("finished");
+
+            // res.status(200).sendFile(pathToPdf, options)
+            console.log("file is sent");
+          }, 5000);
+        }
+        renameAndZip(req.body.names, req.body.files);
+      }
+    } else if (!Array.isArray(req.body.files)) {
+      res.status(400).json({
+        error: "Please be sure that the type is []Array",
+      });
+    }
+  } else {
+    res.status(299).json({
+      error:
+        'There was an error, please be sure that you send of form {"files": []Array}',
+    });
   }
 });
 
--- src/App.js
+++ src/App.js
@@ -1,157 +1,250 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React, { useState, useEffect, useMemo } from "react";
-import "./App.scss";
-import XLSX from "xlsx";
-import axios from "axios";
-import { useReturnArray } from "./hooks/useReturnArray";
-import { useReturnArrayOfPdfs } from "./hooks/useReturnArrayOfPdfs";
+import React, { useState, useEffect } from 'react';
+import './App.scss';
+import XLSX from 'xlsx';
+import axios from 'axios'
+
+var objectForDownload = null
+var arrayForDownload = []
 
 function App() {
-  const [arrayOfSplitPdfs, setArrayOfSplitPdfs] = useState([]);
-  const [trigger, {objectForDownload, loading}] = useReturnArray()
-  const [pdfTrigger, {arrayOfPdfs, isLoading}] = useReturnArrayOfPdfs()
+  const [ arrayOfData, setArrayOfData ] = useState([])
+  const [ displayArray, setDisplayArray ] = useState([])
+  const [ arrayForDownloading, setArrayForDownloading ] = useState([])
+  const [ objectForDownloading, setObjectForDownloading ] = useState(null)
+  const [ downloadGate, setDownloadGate ] = useState(false)
+  const [ arrayOfUrl, setArrayOfUrl ] = useState([])
+  const [ displayArrayOfUrl, SetDisplayArrayOfUrl ] = useState([])
+  const [ blockerDiv, setBlockerDiv ] = useState(null)
+  const [ blockerDivGate, setBlockerDivGate ] = useState(true)
+  const [ loader, setLoader ] = useState(null)
+  const [ loadGate, setLoadGate ] = useState(0)
+  const [ inputLabel, setInputLabel ] = useState('Select a Spreadsheet')
 
-  // wake up the backend -> not needed for local
-  useEffect(() => {
-    axios.get("https://arcane-brook-64097.herokuapp.com/").then((response) => {
-      console.log("response: resoponse")
-    });
-  }, []);
+  // wake up the backend
+  useEffect( () => {
+    axios.get('https://arcane-brook-64097.herokuapp.com/').then( response => {
+      if (response.data) {
+        console.log(response.data)
+      }
+    })
+  }, [])
 
   // Upload and split PDF's
   const handleUploadpdf = (e) => {
     if (e.target.files[0].size > 0) {
-      // Create form data
-      const data = new FormData();
-      data.append("file", e.target.files[0]);
+      // create form data
+      const data = new FormData()
+      data.append('file', e.target.files[0])
 
-      // Split it via the api
-      axios
-        .post(
-          "https://v2.convertapi.com/convert/pdf/to/split?Secret=Ho2xAPDeKwuiTNH7&StoreFile=true",
-          data
-        )
-        .then((response) => {
-          if (response.status === 200) {
-            setArrayOfSplitPdfs(response.data.Files);
-          }
-        });
+      // split it via the api
+      axios.post('https://v2.convertapi.com/convert/pdf/to/split?Secret=LWvPUHAaYjdklEQ7&StoreFile=true', data).then( response => {
+        if (response.status === 200) {
+          setArrayForDownloading(response.data.Files)
+          arrayForDownload = response.data.Files
+        }
+      }) 
     }
-  };
+  }
 
+  useEffect( () => {
+    if (blockerDivGate) {
+      setBlockerDiv(
+        null
+      )
+    } else {
+      setBlockerDiv(
+        <form className='forms'>
+          <h3 className='innerLabel'>
+            Input Pdf
+          </h3>
+          <div>
+            <input type="file" name="file" placeholder="Upload an image" className='inputButton' onChange={(e) => handleUploadpdf(e)}/>
+          </div>
+        </form>
+      )
+    }
+  }, [blockerDivGate])
 
-  // Handles grabbing the excel data and throws it into an array
-  const handleUpload = (e) => {
-    e.preventDefault();
-    let file = e.target.files[0];
-    const reader = new FileReader();
-    reader.onload = (evt) => {
-      // evt = on_file_select event
-      /* Convert to byte stream */
-      const bstr = evt.target.result;
-      const wb = XLSX.read(bstr, { type: "binary" });
-
-      /* Get first worksheet */
-      const wsname = wb.SheetNames[0];
-      const ws = wb.Sheets[wsname];
-
-      /* If not csv, convert to csv */
-      const data = XLSX.utils.sheet_to_csv(ws, { header: 1 });
-
-      /* Split each item into an array based on last expected field */
-      const arrayOfStringifiedSplitXcelData = data.split("Draft");
-
-      // Call hook to get names from the split csv
-      trigger(arrayOfStringifiedSplitXcelData)
-    };
-    reader.readAsBinaryString(file);
-  };
-
-  // When data from first hook returns, and user inputs pdf call second hook
-  useEffect(() => {
-    if (objectForDownload) {
-      if (Object.entries(objectForDownload).length > 0 && arrayOfSplitPdfs.length > 0) {
-        pdfTrigger(arrayOfSplitPdfs, objectForDownload)
+  // spreadsheet part
+  useEffect( () => {
+    let finishedObject = {}
+    let displayArray = []
+    if (arrayOfData) {
+      arrayOfData.forEach( (row, rowId) => {
+        if (rowId !== 0) {
+          let tempArray = row.match(/\S[^,]+/g)
+          console.log(tempArray)
+          if (tempArray) {
+            finishedObject[tempArray[0]] = tempArray[1].replace(/,/, '')
+          }
+        }
+      })
+    }
+    if (finishedObject !== null) {
+      for (const key in finishedObject) {
+        displayArray.push(
+          <h3 className='nameInList'>
+            {key}: {finishedObject[key]}
+          </h3>
+        )
       }
     }
-  }, [objectForDownload, arrayOfSplitPdfs])
+    setDisplayArray(displayArray)
+    setObjectForDownloading(finishedObject)
+    objectForDownload = finishedObject
+    if (Object.keys(finishedObject).length > 0) {
+      setInputLabel('Spreadsheet Selected')
+      setBlockerDivGate(false)
+      setDownloadGate(true)
+    }
+  }, [arrayOfData])
 
-  const render = useMemo( () => {
-    // Block entry of pdf before csv
-    const blocker = Object.entries(objectForDownload).length === 0? null : (
-      <form className="forms">
-        <h3 className="innerLabel">Input Pdf</h3>
-        <div>
-          <input
-            type="file"
-            name="file"
-            placeholder="Upload an image"
-            className="inputButton"
-            onChange={(e) => handleUploadpdf(e)}
-          />
-        </div>
-      </form>
-    )
+  // handles grabbing the excel data and throws it into an array
+  const handleUpload = (e) => {
+    e.preventDefault()
+    let file = e.target.files[0]
+    const reader = new FileReader();
+    reader.onload = (evt) => { 
+      // evt = on_file_select event
+        /* Parse data */
+        const bstr = evt.target.result;
+        const wb = XLSX.read(bstr, {type:'binary'});
+        /* Get first worksheet */
+        const wsname = wb.SheetNames[0];
+        const ws = wb.Sheets[wsname];
+        /* Convert array of arrays */
+        const data = XLSX.utils.sheet_to_csv(ws, {header:1});
+        /* Update state */
+        let myArray = data.split('Draft')
+        console.log(myArray)
+        setArrayOfData(myArray)
+    };
+    reader.readAsBinaryString(file);
+  }
 
-    // Show loading screen for zipping process
-    const loader = loading === 1  && isLoading ? (
-        <div className="loadContainer">
+  // get the pdf after they are split
+  useEffect( () => {
+    let arrayForZip = []
+    async function getData(url = '') {
+      arrayForZip.push(url)
+      const bloby = await fetch(url).then(r => r.blob());
+      return bloby ? bloby : console.log('no reponse')
+    };
+    if (arrayForDownloading.length > 0) {
+      setLoadGate(2)
+      async function loopData(localArray) {
+        let finalArray = []
+        let localArrayOfNames = []
+        for (const key in objectForDownload) {
+          localArrayOfNames.push(`${key} ${objectForDownload[key]}`)
+        }
+        localArray.forEach( (pdfLocal, pdfLocalId) => {
+          getData(pdfLocal.Url).then( response => {
+            let interestingBlob = URL.createObjectURL(response)
+
+            console.log(interestingBlob)
+              finalArray.push(
+                <a 
+                  href={interestingBlob}
+                  download={localArrayOfNames[pdfLocalId]}
+                  className='hyperLinkStyle'
+                >
+                  {pdfLocalId}: {localArrayOfNames[pdfLocalId]}
+                </a>
+              )
+          })
+        })
+        console.log(localArrayOfNames, finalArray)
+        setTimeout(() => {
+          setLoadGate(1)
+          finalArray.sort((a, b) => (a.props.children[0] > b.props.children[0] ? 1 : -1))
+          SetDisplayArrayOfUrl(finalArray)
+          // get the zip
+          async function postData(url = '', data = {}) {
+            // Default options are marked with *
+            const response = await fetch(url, {
+              method: 'POST', // *GET, POST, PUT, DELETE, etc.
+              mode: 'cors', // no-cors, *cors, same-origin
+              cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+              headers: {
+                'Content-Type': 'application/json'
+              },
+              body: JSON.stringify(data)
+            });
+            // return response.json(); // parses JSON response into native JavaScript objects
+          }
+          // https://arcane-brook-64097.herokuapp.com
+          console.log('arrayForZip: ', arrayForZip)
+          console.log('arrayOfNames: ', localArrayOfNames)
+          postData('http://localhost:3001/zip/', {
+            files: arrayForZip,
+            names: localArrayOfNames
+          }).then( response => {
+            window.open('http://localhost:3001/tester/')
+          })
+        }, 7000);
+        return finalArray
+      }
+      loopData(arrayForDownloading).then( response => {
+        console.log(response)
+      })
+    }
+  }, [arrayForDownloading])
+
+  useEffect( () => {
+    if (loadGate === 2) {
+      setLoader(
+        <div className='loadContainer'>
           <div id="container">
             <div className="divider" aria-hidden="true"></div>
             <p className="loading-text" aria-label="Loading">
-              <span className="letter" aria-hidden="true">
-                L
-              </span>
-              <span className="letter" aria-hidden="true">
-                o
-              </span>
-              <span className="letter" aria-hidden="true">
-                a
-              </span>
-              <span className="letter" aria-hidden="true">
-                d
-              </span>
-              <span className="letter" aria-hidden="true">
-                i
-              </span>
-              <span className="letter" aria-hidden="true">
-                n
-              </span>
-              <span className="letter" aria-hidden="true">
-                g
-              </span>
+              <span className="letter" aria-hidden="true">L</span>
+              <span className="letter" aria-hidden="true">o</span>
+              <span className="letter" aria-hidden="true">a</span>
+              <span className="letter" aria-hidden="true">d</span>
+              <span className="letter" aria-hidden="true">i</span>
+              <span className="letter" aria-hidden="true">n</span>
+              <span className="letter" aria-hidden="true">g</span>
             </p>
           </div>
         </div>
-      ) : arrayOfPdfs.length > 0 ? (
-        <div className="pdfDownloadContainer">
-          <h3 className="innerLabel">Renamed Pdfs</h3>
-          {arrayOfPdfs}
+      )
+    } else if (loadGate === 1) {
+      setLoader(
+        <div className='pdfDownloadContainer'>
+          <h3 className='innerLabel'>
+            Renamed Pdfs
+          </h3>
+          {displayArrayOfUrl}
         </div>
-      ) : null
+      )
+    } else {
+      setLoader(null)
+    }
+  }, [displayArrayOfUrl, loadGate])
 
-      // Content
-    return (
-      <div className="App">
-        <div className="whiteOverlay">
-          <div className="containerOne">
-            <h1 className="mainTitle">H2O Pdf Splitter</h1>
-            <div className="inputContainer">
-              <form className="forms">
-                <h3 className="innerLabel">Select a Spreadsheet</h3>
-                <div>
-                  <input
-                    type="file"
-                    name="file"
-                    placeholder="Upload Spreadsheet"
-                    className="inputButton"
-                    onChange={(e) => handleUpload(e)}
-                  />
-                </div>
-              </form>
-              {blocker}
-            </div>
-            {loader}
+  return (
+    <div className="App">
+      <div className='whiteOverlay'>
+        <div className='containerOne'>
+          <h1 className='mainTitle'>H2O Pdf Splitter</h1>
+          <div className='inputContainer'>
+            <form className='forms'>
+              <h3 className='innerLabel'>
+                {inputLabel}
+              </h3>
+              <div>
+                <input 
+                  type="file" 
+                  name="file" 
+                  placeholder="Upload Spreadsheet" 
+                  className='inputButton' 
+                  onChange={(e) => handleUpload(e)}
+                />
+              </div>
+            </form>
+            {blockerDiv}
           </div>
         </div>
       </div>
